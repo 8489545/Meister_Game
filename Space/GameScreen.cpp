4ff558c04f9acba28a0cc5e5bcd669c9c921d5c3
8489545 <rebob9545@gmail.com>
2020-05-20 16:59:00 +0900

    검정색 레터박스 만듬

--- Space/GameScreen.cpp
+++ Space/GameScreen.cpp
@@ -3,6 +3,11 @@
 
 void GameScreen::Init()
 {
+	m_RBox = Sprite::Create(L"Painting/GameScreen/BlackBox.png");
+	m_RBox->SetPosition(660 / 2, 1080 / 2);
+
+	m_LBox = Sprite::Create(L"Painting/GameScreen/BlackBox.png");
+	m_LBox->SetPosition(3180 / 2, 1080 / 2);
 }
 
 void GameScreen::Release()
@@ -15,4 +20,6 @@ void GameScreen::Update(float deltaTime, float time)
 
 void GameScreen::Render()
 {
+	m_RBox->Render();
+	m_LBox->Render();
 }
