9eecb9c1b7f5a6b44a05e2768ae33ef45213c544
김 동규 <rebob9545@gmail.com>
2020-05-23 17:15:03 +0900

    배경 스크롤

--- Space/GameScreen.cpp
+++ Space/GameScreen.cpp
@@ -3,6 +3,11 @@
 
 void GameScreen::Init()
 {
+	m_GameScreen1 = Sprite::Create(L"Painting/GameScreen/Stage1.png");
+	m_GameScreen1->SetPosition(1920 / 2, 1080 / 2);
+	m_GameScreen2 = Sprite::Create(L"Painting/GameScreen/Stage1.png");
+	m_GameScreen2->SetPosition(1920 / 2, m_GameScreen1->m_Position.y - 1080);
+
 	m_RBox = Sprite::Create(L"Painting/GameScreen/BlackBox.png");
 	m_RBox->SetPosition(660 / 2, 1080 / 2);
 
@@ -20,10 +25,23 @@ void GameScreen::Release()
 
 void GameScreen::Update(float deltaTime, float time)
 {
+	m_GameScreen1->m_Position.y += 500 * dt;
+	m_GameScreen2->m_Position.y += 500 * dt;
+
+	if (m_GameScreen1->m_Position.y >= 1080 + (m_GameScreen1->m_Size.y / 2))
+	{
+		m_GameScreen1->m_Position.y = m_GameScreen2->m_Position.y - 1080;
+	}
+	if (m_GameScreen2->m_Position.y >= 1080 + (m_GameScreen2->m_Size.y / 2))
+	{
+		m_GameScreen2->m_Position.y = m_GameScreen1->m_Position.y - 1080;
+	}
 }
 
 void GameScreen::Render()
 {
 	m_RBox->Render();
 	m_LBox->Render();
+	m_GameScreen1->Render();
+	m_GameScreen2->Render();
 }
