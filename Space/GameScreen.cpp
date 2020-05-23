4c80ca2ff348dbec61bad5898e6d1b1e534830da
김 동규 <rebob9545@gmail.com>
2020-05-23 21:20:36 +0900

    튜토리얼 제작

--- Space/GameScreen.cpp
+++ Space/GameScreen.cpp
@@ -8,15 +8,20 @@ void GameScreen::Init()
 	m_GameScreen2 = Sprite::Create(L"Painting/GameScreen/Stage1.png");
 	m_GameScreen2->SetPosition(1920 / 2, m_GameScreen1->m_Position.y - 1080);
 
+	m_Tutorials = Sprite::Create(L"Painting/GameScreen/Tutorials.png");
+	m_Tutorials->SetPosition(1920 / 2, 1080 / 2);
+
 	m_RBox = Sprite::Create(L"Painting/GameScreen/BlackBox.png");
 	m_RBox->SetPosition(660 / 2, 1080 / 2);
 
 	m_LBox = Sprite::Create(L"Painting/GameScreen/BlackBox.png");
 	m_LBox->SetPosition(3180 / 2, 1080 / 2);
 
-	GameMgr::GetInst()->CreatePlayer();
-
 	GameMgr::GetInst()->SetLimit(1260 - 114 / 2, 660 + 114 / 2, 1080 - 110 / 2, 0 + 110 / 2);
+
+	ObjMgr->AddObject(m_Tutorials, "UI");
+
+	m_isStart = false;
 }
 
 void GameScreen::Release()
@@ -35,6 +40,14 @@ void GameScreen::Update(float deltaTime, float time)
 	if (m_GameScreen2->m_Position.y >= 1080 + (m_GameScreen2->m_Size.y / 2))
 	{
 		m_GameScreen2->m_Position.y = m_GameScreen1->m_Position.y - 1080;
+	}
+
+	if (INPUT->GetButtonDown())
+	{
+		m_Tutorials->SetDestroy(true);
+		GameMgr::GetInst()->CreatePlayer();
+		m_isStart = true;
+		INPUT->ButtonDown(false);
 	}
 }
 
--- Space/GameScreen.h
+++ Space/GameScreen.h
@@ -4,12 +4,16 @@ class GameScreen : public Scene
 	Sprite* m_GameScreen1;
 	Sprite* m_GameScreen2;
 
+	Sprite* m_Tutorials;
+
 	Sprite* m_RBox;
 	Sprite* m_LBox;
 public:
 
 	void Init();
 	void Release();
+
+	bool m_isStart;
 
 	void Update(float deltaTime, float time);
 	void Render();
