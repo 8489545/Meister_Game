a857d0039c90fb358b0aeef5b9e7f8bce3ca4857
8489545 <rebob9545@gmail.com>
2020-06-11 20:07:30 +0900

    보스 프로펠러 6개 장착및 위치조정

--- Space/FinalBoss.cpp
+++ Space/FinalBoss.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "FinalBoss.h"
 
+//200% > 150%
+
 FinalBoss::FinalBoss()
 {
 	m_Body = Sprite::Create(L"Painting/Object/Enemy/FinalBoss/Body.png");
@@ -11,10 +13,42 @@ FinalBoss::FinalBoss()
 	m_RWingCol = Sprite::Create(L"Painting/Object/Enemy/FinalBoss/WingCol.png");
 	m_TailWingCol = Sprite::Create(L"Painting/Object/Enemy/FinalBoss/TailWingCol.png");
 
+	m_LProp1 = new Animation();
+	m_LProp2 = new Animation();
+	m_LProp3 = new Animation();
+
+	m_RProp1 = new Animation();
+	m_RProp2 = new Animation();
+	m_RProp3 = new Animation();
+
+	m_LProp1->Init(1, true);
+	m_LProp2->Init(1, true);
+	m_LProp3->Init(1, true);
+
+	m_RProp1->Init(1, true);
+	m_RProp2->Init(1, true);
+	m_RProp3->Init(1, true);
+
+	m_LProp1->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+	m_LProp2->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+	m_LProp3->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+
+	m_RProp1->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+	m_RProp2->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+	m_RProp3->AddContinueFrame(L"Painting/Object/Enemy/FinalBoss/Animation/", 1, 8);
+
 	m_BodyCol->m_Visible = false;
 	m_LWingCol->m_Visible = false;
 	m_RWingCol->m_Visible = false;
 	m_TailWingCol->m_Visible = false;
+
+	ObjMgr->AddObject(m_LProp1, "Effect");
+	ObjMgr->AddObject(m_LProp2, "Effect");
+	ObjMgr->AddObject(m_LProp3, "Effect");
+
+	ObjMgr->AddObject(m_RProp1, "Effect");
+	ObjMgr->AddObject(m_RProp2, "Effect");
+	ObjMgr->AddObject(m_RProp3, "Effect");
 
 	ObjMgr->AddObject(m_BodyCol, "BossCol");
 	ObjMgr->AddObject(m_LWingCol, "BossCol");
@@ -35,7 +69,7 @@ void FinalBoss::CollisionCheak()
 void FinalBoss::Collision()
 {
 	m_Body->B = 0;
-	m_Body->G = 0;
+	m_Body->G = 0; 
 }
 
 void FinalBoss::Update(float deltatime, float Time)
@@ -114,4 +148,12 @@ void FinalBoss::SetObjectsPosition()
 	m_LWingCol->SetPosition(m_Position.x - 350, m_Position.y - 70);
 	m_RWingCol->SetPosition(m_Position.x + 350, m_Position.y - 70);
 	m_TailWingCol->SetPosition(m_Position.x, m_Position.y + 230);
+
+	m_LProp1->SetPosition(m_Position.x - 495, m_Position.y  - 180);
+	m_LProp2->SetPosition(m_Position.x - 355, m_Position.y  - 180);
+	m_LProp3->SetPosition(m_Position.x - 205, m_Position.y  - 180);
+
+	m_RProp1->SetPosition(m_Position.x + 495 + 15, m_Position.y  - 180);
+	m_RProp2->SetPosition(m_Position.x + 355 + 15, m_Position.y  - 180);
+	m_RProp3->SetPosition(m_Position.x + 205 + 15, m_Position.y  - 180);
 }
--- Space/FinalBoss.h
+++ Space/FinalBoss.h
@@ -7,6 +7,13 @@ class FinalBoss : public Object
 	Sprite* m_LWingCol;
 	Sprite* m_RWingCol;
 	Sprite* m_TailWingCol;
+
+	Animation* m_LProp1;
+	Animation* m_LProp2;
+	Animation* m_LProp3;
+	Animation* m_RProp1;
+	Animation* m_RProp2;
+	Animation* m_RProp3;
 public:
 	FinalBoss();
 	~FinalBoss();
